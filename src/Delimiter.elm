2c4a0c4dc3ec11bda6bac0eb26cf89398f740906
Travis Wright <travis@psyonix.com>
2016-12-29 10:48:29 -0800

    Refactor Delimiter algorithm to be simpler and make better use of functional programming

--- src/Delimiter.elm
+++ src/Delimiter.elm
@@ -1,35 +1,17 @@
 module Delimiter exposing (format)
 
 
-type alias FormatParams =
-    { interval : Int
-    , string : String
-    }
-
-
 format : Int -> String -> String -> String
 format interval delimiter formatStr =
-    formatInterval (FormatParams interval delimiter) "" formatStr
+    splitByInterval interval formatStr
+        |> List.reverse
+        |> String.join delimiter
 
 
-formatInterval : FormatParams -> String -> String -> String
-formatInterval del formatStr inStr =
-    if (String.length inStr == 0) then
-        formatStr
+splitByInterval : Int -> String -> List String
+splitByInterval interval formatStr =
+    if (String.length formatStr == 0) then
+        []
     else
-        let
-            newFormatStr =
-                (String.right del.interval inStr) ++ (conditionalAddDelimiter del.string formatStr)
-
-            remainingStr =
-                String.left (String.length inStr - del.interval) inStr
-        in
-            formatInterval del newFormatStr remainingStr
-
-
-conditionalAddDelimiter : String -> String -> String
-conditionalAddDelimiter delString val =
-    if (String.length val == 0) then
-        val
-    else
-        delString ++ val
+        splitByInterval interval (String.dropRight interval formatStr)
+            |> List.append [ String.right interval formatStr ]
