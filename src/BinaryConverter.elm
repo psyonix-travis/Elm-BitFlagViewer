0c09dbef3699a37d5d712c50b2379c9a0cc9bb8e
Travis Wright <travis@psyonix.com>
2017-01-05 00:12:27 -0800

    Update binary converter to be able to convert a list of bit digits to a string Also fix binary formatter to not add an extra set of 0s when the incoming string is the right length already

--- src/BinaryConverter.elm
+++ src/BinaryConverter.elm
@@ -1,29 +1,29 @@
-module BinaryConverter exposing (convert, convertToList)
+module BinaryConverter exposing (int, intToList, listToString)
 
 import Bitwise
 
 
-convert : Int -> String
-convert value =
-    internalConvertList (abs value)
+int : Int -> String
+int value =
+    internalIntList (abs value)
         |> List.reverse
         |> List.map (\x -> toString x)
         |> String.join ""
 
 
-convertToList : Int -> List Int
-convertToList value =
-    internalConvertList value
+intToList : Int -> List Int
+intToList value =
+    internalIntList value
         |> List.indexedMap (,)
         |> List.filterMap digitFilter
 
 
-internalConvertList : Int -> List Int
-internalConvertList value =
+internalIntList : Int -> List Int
+internalIntList value =
     if value <= 1 then
         [ (getLSB value) ]
     else
-        (getLSB value) :: (internalConvertList (Bitwise.shiftRightBy 1 value))
+        (getLSB value) :: (internalIntList (Bitwise.shiftRightBy 1 value))
 
 
 getLSB : Int -> Int
@@ -40,3 +40,34 @@ digitFilter ( index, value ) =
         Just (index + 1)
     else
         Nothing
+
+
+listToString : List Int -> String
+listToString list =
+    listToStringIter 1 list
+
+
+listToStringIter : Int -> List Int -> String
+listToStringIter count list =
+    case List.head list of
+        Just value ->
+            listToStringIter (count + 1) (listToStringConditionalDrop value count list) ++ (listValueToStr value count)
+
+        Nothing ->
+            ""
+
+
+listToStringConditionalDrop : Int -> Int -> List Int -> List Int
+listToStringConditionalDrop value count list =
+    if (value == count) then
+        (List.drop 1 list)
+    else
+        list
+
+
+listValueToStr : Int -> Int -> String
+listValueToStr value count =
+    if (value == count) then
+        "1"
+    else
+        "0"
